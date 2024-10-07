3804abe72b778c4f13c9aaca88d3acbacc6c3290
yeon-dong <jae073@naver.com>
2024-10-07 17:12:56 +0900

    .

---
+++ Ex07_mybatis-service/src/main/java/com/sample/spring/Ex07MybatisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sample.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex07MybatisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex07MybatisServiceApplication.class, args);
+	}
+
+}
--- Ex07_mybatis-service/src/main/java/com/sample/spring/service/ISimpleBbsService.java
+++ Ex07_mybatis-service/src/main/java/com/sample/spring/service/ISimpleBbsService.java
@@ -1,5 +1,15 @@
 package com.sample.spring.service;
 
-public interface ISimpleBbsService {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.sample.spring.dto.SimpleBbsDto;
+
+public interface ISimpleBbsService {
+	public List<SimpleBbsDto> listDao();
+	public int countDao();
+	public SimpleBbsDto view(String id);
+	public int writeDao(String writer,String title, String content);
+	public int delete(@Param("_id") String id);   //delete?id=1
 }
