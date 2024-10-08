f213bb179d8dbe1166ac80008f63491bcd4665c6
yeon-dong <jae073@naver.com>
2024-10-08 16:48:04 +0900

    api 구조

---
+++ Ex09_API/src/main/java/com/sample/spring/controller/Mycontroller.java
@@ -0,0 +1,54 @@
+package com.sample.spring.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Mycontroller {
+	@GetMapping("/hello")
+	public String hello() {
+		return "get-hello";
+	}
+	@PostMapping("/hello")
+	public String posthello() {
+		return "post-hello";
+	}
+	@PutMapping("/hello")
+	public String puthello() {
+		return "put-hello";
+	}
+	@DeleteMapping("/hello")
+	public String deletehello() {
+		return "delete-hello";
+	}
+	@GetMapping("/test/param")
+	public String requestParam(
+			@RequestParam("name") String name,
+			@RequestParam("age") Integer age
+			) {
+
+		return "hello, requestParam, i am " + name +"," + age;
+	}
+	@GetMapping("/test/path/{name}/{age}")
+	public String requestPathVariable(
+			@PathVariable("name") String name,
+			@PathVariable("age") Integer age
+			) {
+
+		return "hello, requestParam, i am " + name +"," + age;
+	}
+	@PostMapping
+	public String requsestBody(
+			@RequestBody RequestDto request
+			) {
+		return "hello, request path, i am " + request.getName() + " , " + request.getAge();
+	}
+	
+	
+}
