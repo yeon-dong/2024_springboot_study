04833fdcd51d36668a3ab3e217f13d167d5b1080
yeon-dong <jae073@naver.com>
2024-10-11 15:29:03 +0900

    ex11 study

--- Ex10_JPA_CRUD/src/main/java/com/sample/spring/controller/TestApi.java
+++ Ex10_JPA_CRUD/src/main/java/com/sample/spring/controller/TestApi.java
@@ -1,16 +1,21 @@
 package com.sample.spring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sample.spring.entity.TestEntity;
 import com.sample.spring.service.TestService;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Log4j2
 @Slf4j
 @RestController
 public class TestApi {
@@ -48,6 +54,17 @@ public class TestApi {
 	public void deleteUpdate(@PathVariable("id") Long id) {
 		testService.delete(id);
 		//@RequestParam("id") Long id 로 받아와서 할 수도 있음. 그러면 ?id=2 이런식으로 query로 가져와야 함
+	}
+	
+
+//	public List<TestEntity> jsonData(){
+//		return testService.findAll(); // 200 ok
+//		
+//	}
+	@GetMapping("/test")
+	public ResponseEntity<List<TestEntity>> jsonData(){
+		List<TestEntity> data = testService.findAll();
+		return ResponseEntity.ok(data);
 	}
 	
 	@AllArgsConstructor
--- Ex10_JPA_CRUD/src/main/java/com/sample/spring/service/TestService.java
+++ Ex10_JPA_CRUD/src/main/java/com/sample/spring/service/TestService.java
@@ -1,6 +1,9 @@
 package com.sample.spring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.sample.spring.entity.TestEntity;
@@ -26,5 +29,9 @@ public class TestService {
 	public void delete(Long id) {
 		TestEntity testEntity = testRepository.findById(id).get();
 		testRepository.delete(testEntity);
+	}
+	
+	public List<TestEntity> findAll(){
+		return testRepository.findAll(Sort.by(Sort.Direction.DESC,"id"));
 	}
 }
