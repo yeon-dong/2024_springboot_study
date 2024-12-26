118a62296d75faa25579783741e9c94886dba0c9
yeon-dong <jae073@naver.com>
2024-12-26 10:26:10 +0900

    Testing 공부 중

---
+++ Testing/hdauto/jtest/src/main/java/com/hd/JtestApplication.java
@@ -0,0 +1,13 @@
+package com.hd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtestApplication.class, args);
+	}
+
+}
---
+++ Testing/hdauto/jtest/src/test/java/com/hd/JtestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hd;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class JtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+		log.info("test log");
+	}
+
+}
